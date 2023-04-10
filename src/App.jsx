cbec9aeaf8830b9e157277ebe3321ca3f0b7832f
Pisit Wongsripisant <act21078@gmail.com>
2023-04-10 15:50:06 +0700

    connect register api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import ChatPage from "./pages/ChatPage/ChatPage";
 import ProtectedRoutesAuth from "./middleware/ProtectedRoutesAuth";
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <header className="app-header">React Chat</header>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route element={<ProtectedRoutesAuth />}>
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./RegisterPage.css";
+import axios from "axios";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
-//   const [username, setUsername] = useState("");
+  //   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-//   const handleUsernameChange = (e) => {
-//     setUsername(e.target.value);
-//   };
+  //   const handleUsernameChange = (e) => {
+  //     setUsername(e.target.value);
+  //   };
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -20,13 +21,23 @@ const RegisterPage = () => {
     setPassword(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // w8 for implement here #P JojO
+    // w8 for implement here #P JoJo
     // Here you can add your code to send a POST request to the server to register the user
     // with the provided username, email, and password
     // Once the user is registered, you can navigate to a different page using `navigate('/path')`
-    navigate('/login');
+    await axios
+      .post(`${process.env.REACT_APP_API_URL}/auth/register`, {
+        email,
+        password,
+      })
+      .then((res) => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        alert("register failed");
+      });
   };
 
   return (
@@ -73,11 +84,10 @@ const RegisterPage = () => {
         </div>
 
         <div className="button-resgiter-panel">
-            <button type="submit" className="button-submit-register">
-                Register
-            </button>  
+          <button type="submit" className="button-submit-register">
+            Register
+          </button>
         </div>
-      
       </form>
     </div>
   );
